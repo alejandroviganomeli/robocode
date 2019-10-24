7d65b2a218dbaeef4b8b2a90b0c6f3505f9ef7fc
alejandrovigano <alejandro.vigano@mercadolibre.com>
2019-10-24 15:54:10 -0300

    init

--- src/main/java/VFex.java
+++ src/main/java/VFex.java
@@ -1,9 +1,13 @@
-package ar.com.mercadolibre.reporting.robocode;
-
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
+import java.awt.*;
+
 public class VFex extends Robot {
+
+    public VFex() {
+        this.setColors(Color.BLACK, Color.BLACK, Color.BLACK);
+    }
 
     public void run() {
         while (true) {
